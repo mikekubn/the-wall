61839163c5b4c2eecb87a709ebffb5b518c1a84f
Michael Kubin <michael.kubn@gmail.com>
2024-11-19 20:34:38 +0100

    fix: post component

--- components/post.tsx
+++ components/post.tsx
@@ -16,25 +16,25 @@ const Post: React.FC<{ post: PostProps }> = ({ post }) => {
   const sort = params.get('sort');
 
   return (
-    <Link
-      href={`/?sort=${sort}&id=${post?.id}`}
-      className="relative flex flex-col w-[314px] h-[167px] md:w-[406px] md:h-[251px] p-4 md:p-12 rounded-[12px] bg-white hover:bg-blue border group border-gray_5">
-      <p className="font-semibold font-inter text-gray_2 text-[12px] md:text-[16px] group-hover:text-white">{post?.role}:</p>
-      <p className="min-h-[54px] md:min-h-[58px] font-semibold font-inter text-[22px] md:text-[28px] my-2.5 leading-none line-clamp-2 group-hover:text-white">
-        <span className="italic mr-1">&quot;</span>
-        <span>{post.message}</span>
-        <span className="italic ml-1">&quot;</span>
-      </p>
-      <p className="font-semibold font-inter text-gray_3 text-[12px] md:text-[16px] group-hover:text-white">{formattedDate}</p>
-      <div className="absolute size-full inset-0 rounded-[12px]">
-        <div className="flex flex-1 size-full items-end justify-end rounded-[12px] p-4 md:p-12">
-          <div className="flex flex-row gap-4 items-center">
-            <Rating post={post} />
-            <ShareButton id={post?.id} />
-          </div>
+    <div className="relative">
+      <Link
+        href={`/?sort=${sort}&id=${post?.id}`}
+        className="relative flex flex-col w-[314px] h-[167px] md:w-[406px] md:h-[251px] p-4 md:p-12 rounded-[12px] bg-white hover:bg-blue border group border-gray_5">
+        <p className="font-semibold font-inter text-gray_2 text-[12px] md:text-[16px] group-hover:text-white">{post?.role}:</p>
+        <p className="min-h-[54px] md:min-h-[58px] font-semibold font-inter text-[22px] md:text-[28px] my-2.5 leading-none line-clamp-2 group-hover:text-white">
+          <span className="italic mr-1">&quot;</span>
+          <span>{post.message}</span>
+          <span className="italic ml-1">&quot;</span>
+        </p>
+        <p className="font-semibold font-inter text-gray_3 text-[12px] md:text-[16px] group-hover:text-white">{formattedDate}</p>
+      </Link>
+      <div className="absolute bottom-12 right-0 size-fit pr-4 md:pr-12">
+        <div className="flex flex-row gap-4 items-center justify-end">
+          <Rating post={post} />
+          <ShareButton id={post?.id} />
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
